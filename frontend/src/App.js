437c821462192797fb069e3a580191d1bbe70905
Amey Kusurkar <amey1000@gmail.com>
2021-02-23 23:49:02 +0000

    Add turn playing buttons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,14 +6,19 @@ function App() {
   const [playerID, setPlayerID] = useState("");
   const [player, setPlayer] = useState({ hand: [] });
 
-  useEffect(refreshGame, []);
+  useEffect(() => {
+    const interval = setInterval(refreshGame, 2000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
   useEffect(() => {
     fetch(`http://localhost:5000/api/players/${playerID}`)
       .then(handleBadRequest)
       .then((data) => setPlayer(data))
       .catch((response) => console.log(response));
-  }, [playerID]);
+  }, [playerID, game.current_player_id]);
 
   function handleClick(e) {
     setPlayerID(e.target.innerHTML);
@@ -26,9 +31,30 @@ function App() {
       .catch((response) => console.log(response));
   }
 
+  function isMyTurn() {
+    return game.current_player_id === playerID;
+  }
+
+  function playTurn(move, cardValue) {
+    fetch("http://localhost:5000/api/game/play", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        move: move,
+        card_value: cardValue,
+        player_id: playerID,
+      }),
+    })
+      .then(handleBadRequest)
+      .then((data) => setGame(data.game))
+      .catch((response) => console.log(response));
+  }
+
   return (
     <>
+      <h1>President</h1>
       <p>Whose turn: {game.current_player_id}</p>
+      <p>Last card: {game.top_card && game.top_card.description}</p>
       <h2>Players</h2>
       <div>
         {game.player_ids.map((id) => (
@@ -39,14 +65,31 @@ function App() {
       </div>
       <h2>You</h2>
       <div>Player ID: {playerID}</div>
+      <div>
+        <button
+          disabled={!isMyTurn()}
+          onClick={() => {
+            playTurn("PASS", 0);
+          }}
+        >
+          PASS
+        </button>
+      </div>
       <h3>Cards</h3>
-      <ul>
+      <div>
         {player.hand.map((card) => (
-          <li key={card.value}>
-            {card.description + (card.playable ? " (playable)" : "")}
-          </li>
+          <div key={card.value}>
+            <button
+              disabled={!isMyTurn() || !card.playable}
+              onClick={() => {
+                playTurn("PLAY", card.value);
+              }}
+            >
+              {card.description}
+            </button>
+          </div>
         ))}
-      </ul>
+      </div>
     </>
   );
 }
